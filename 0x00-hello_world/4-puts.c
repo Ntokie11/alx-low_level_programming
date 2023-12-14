d173a13e4e77f50b1a59225c7d0aa1eae7567086
Ntokozo Matshoza <ntokozomatshoza@gmail.com>
2023-12-14 10:17:26 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints a line exactly using a puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"};
+return (0);
+}
